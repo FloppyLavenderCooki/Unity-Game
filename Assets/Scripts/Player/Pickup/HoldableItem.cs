6e81617d0440e950b9b49edbe223b3184876c8ba
SunnyFlops <arav.prasad@outlook.com>
2025-05-01 19:48:48 +1200

    private refactoring

--- Assets/Scripts/Player/Pickup/HoldableItem.cs
+++ Assets/Scripts/Player/Pickup/HoldableItem.cs
@@ -1,17 +1,17 @@
 using UnityEngine;
 
 public class NoLightHoldableItem : MonoBehaviour {
-    private Vector3 respawnPos;
+    private Vector3 _respawnPos;
     
     void Awake() {
-        respawnPos = transform.position;
+        _respawnPos = transform.position;
     }
 
     void Update() {
         if (transform.position.y < -50f) {
             transform.GetComponent<Rigidbody>().linearVelocity = Vector3.zero;
             transform.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
-            transform.position = respawnPos;
+            transform.position = _respawnPos;
             transform.rotation = Quaternion.identity;
         }
     }
