d25d2a2880d21f7f08cbeae1aa7dc32047281243
Cooki-Studios <59518753+Cooki-Studios@users.noreply.github.com>
2025-06-25 19:32:38 +1200

    Update LoadingUI.cs

--- Assets/Scripts/UI/LoadingUI.cs
+++ Assets/Scripts/UI/LoadingUI.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -6,7 +7,8 @@ namespace UI
     public class LoadingUI : MonoBehaviour
     {
         private static VisualElement _root;
-        
+        private static VisualElement _loadingCover;
+
         private void OnEnable()
         {
             var uiDocument = GetComponent<UIDocument>();
@@ -15,14 +17,33 @@ namespace UI
                 _root = uiDocument.rootVisualElement;
             }
         }
-        
+
         public static void LoadScene(string sceneName)
         {
             if (_root == null) return;
-            
+
             _root.Q<VisualElement>("container").style.visibility = Visibility.Visible;
-            
+            _loadingCover = _root.Q<VisualElement>("loading-cover");
+
             var loading = UnityEngine.SceneManagement.SceneManager.LoadSceneAsync(sceneName);
+            if (loading == null) return;
+
+            MonoBehaviour currentInstance = FindAnyObjectByType<LoadingUI>();
+            if (currentInstance)
+            {
+                currentInstance.StartCoroutine(UpdateLoadingText(loading, _loadingCover));
+            }
+        }
+
+        private static IEnumerator UpdateLoadingText(AsyncOperation operation, VisualElement loadingCover)
+        {
+            while (!operation.isDone)
+            {
+                loadingCover.style.translate =
+                    new StyleTranslate(new Translate(Length.Percent(90 + operation.progress * 35), 0));
+
+                yield return null;
+            }
         }
     }
 }
