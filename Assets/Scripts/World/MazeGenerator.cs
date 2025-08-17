bbd13abcb9f3cb5768a54fde8201b7e0c3471b14
Cooki-Studios <59518753+Cooki-Studios@users.noreply.github.com>
2025-08-17 14:11:51 +1200

    Maze fix part 2.1: A single pillar

--- Assets/Scripts/World/MazeGenerator.cs
+++ Assets/Scripts/World/MazeGenerator.cs
@@ -1,5 +1,4 @@
 using System.Collections.Generic;
-using System.IO;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -8,13 +7,16 @@ namespace World
     public class MazeGenerator : MonoBehaviour
     {
         public GameObject bookshelfModel;
+        public GameObject pillarModel;
         
         private Vector3 _bookshelfSize;
-        private List<List<Dictionary<string, bool>>> _bookshelves = new();
+        private Dictionary<string,bool>[,] _array;
+        private int _rows;
+        private int _cols;
 
         public int level = 3;
-        public float stepSize = 1f;
-        public Vector2 startPosition = Vector2.zero;
+        public int stepSize = 1;
+        public Vector2Int startPosition = Vector2Int.zero;
         private HilbertCurve _hilbert;
         private BookshelfGenerator _bookshelfGenerator;
         private GameObject _books;
@@ -31,16 +33,17 @@ namespace World
             _hilbert = new HilbertCurve(startPosition, stepSize);
             _hilbert.GenerateHilbert(level, HilbertCurve.Direction.Down);
             
-            string path = Application.persistentDataPath + "/points.txt";
-
-            List<string> lines = new List<string>();
-            foreach (Vector2 v in _hilbert.Points)
+            // Make all points positive (HilbertCurve.Direction.Up doesn't work)
+            for (int i = 0; i < _hilbert.Points.Count; i++)
             {
-                lines.Add(v.x + "," + v.y);
+                var p = _hilbert.Points[i];
+                if (p.x < 0) p.x = -p.x;
+                if (p.y < 0) p.y = -p.y;
+                _hilbert.Points[i] = p;
             }
-            File.WriteAllLines(path, lines);
 
-            Debug.Log("List<Vector2> saved to: " + path);
+            GenerateEmptyArray();
+            GenerateMaze(startPosition);
             
             Destroy(GameObject.Find("Loading UI"));
         }
@@ -51,6 +54,56 @@ namespace World
             
             foreach (Transform child in _books.transform) { Destroy(child.gameObject); }
             foreach (Transform child in transform) { Destroy(child.gameObject); }
+        }
+
+        private void GenerateEmptyArray()
+        {
+            int maxX = 0;
+            int maxY = 0;
+            foreach (var p in _hilbert.Points)
+            {
+                if (p.x > maxX) maxX = p.x;
+                if (p.y > maxY) maxY = p.y;
+            }
+            
+            _rows = (maxY + 1) * 2;
+            _cols = (maxX + 1) * 2;
+            
+            _array = new Dictionary<string, bool>[_rows, _cols];
+            for (int y = 0; y < _rows; y++)
+            {
+                for (int x = 0; x < _cols; x++)
+                {
+                    _array[y, x] = new Dictionary<string, bool>
+                    {
+                        { "pillar", false }
+                    };
+                }
+            }
+        }
+        
+        private void GenerateMaze(Vector2Int position)
+        {
+            _array[position.x * 2, position.y * 2]["pillar"] = _hilbert.Points.Contains(position);
+            // Debug.Log(_array);
+            Debug.Log(_rows+", "+_cols);
+            
+            CreateMazeObjects();
+        }
+
+        private void CreateMazeObjects()
+        {
+            for (int y = 0; y < _rows; y++)
+            {
+                for (int x = 0; x < _cols; x++)
+                {
+                    if (_array[y, x]["pillar"])
+                    {
+                        GameObject pillarInstance = Instantiate(pillarModel, transform);
+                        pillarInstance.transform.position += new Vector3(y, 0, x);
+                    }
+                }
+            }
         }
     }
 }
