503bb7e08aa64d6f8e42746afc35cdb5fed53c53
Cooki-Studios <59518753+Cooki-Studios@users.noreply.github.com>
2025-08-26 22:41:44 +1200

    Book name objective with colour

--- Assets/Scripts/World/Objectives.cs
+++ Assets/Scripts/World/Objectives.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.TextCore.Text;
 using UnityEngine.UIElements;
 
 namespace World
@@ -8,14 +10,28 @@ namespace World
         private UIDocument _document;
         private VisualElement _objectives;
         private int _objectiveNum;
-        private string _bookName = "THE BOOK";
+        private Transform _books;
+        public TextColorGradient gradient;
 
         private void Start()
         {
             _document = GetComponent<UIDocument>();
             _objectives = _document.rootVisualElement.Q<VisualElement>("objectives");
+            _books = GameObject.Find("Books").transform;
+        }
 
-            AddObjective($"Get the book: <gradient=\"Book Name Gradient\">{_bookName}</gradient>");
+        private void Update()
+        {
+            if (!_books || _objectives == null || _books.childCount <= 0 || _objectives.childCount != 0
+                || Keyboard.current.rKey.wasPressedThisFrame) return;
+            
+            GameObject book = _books.GetChild(Random.Range(0, _books.childCount)).gameObject;
+            Color bookColor = book.GetComponentInChildren<Renderer>().material.color;
+            if (book.GetComponentInChildren<Renderer>().material.color.grayscale < 0.2) bookColor *= 2.5f;
+            if (book.GetComponentInChildren<Renderer>().material.color.grayscale < 0.01) bookColor = Color.white;
+            gradient.topLeft = gradient.topRight = gradient.bottomLeft = gradient.bottomRight = bookColor;
+            
+            AddObjective($"Get the book: <gradient=\"{gradient.name}\">{book.name}</gradient>");
             AddObjective("Drop it on the kiosk");
             AddObjective("Put it in the box");
         }
